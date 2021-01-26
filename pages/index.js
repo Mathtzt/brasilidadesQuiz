c20a02fe95f72b4dd19de16ceef6af205417c2e1
Mathtzt <matheus.cn10@gmail.com>
2021-01-25 22:39:39 -0300

    Criando component Header para inclusive metadados

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import React from "react";
 import styled from 'styled-components'
 
 import db from '../db.json';
 import Widget from '../src/components/Widget'
 import QuizBackground from '../src/components/QuizBackground'
+import Header from '../src/components/Header'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
 
@@ -19,27 +21,28 @@ export const QuizContainer = styled.div`
 
 export default function Home() {
   return (
-      <QuizBackground backgroundImage={db.bg}>
-        <QuizContainer>
-          <Widget>
-            <Widget.Header>
-              <h1>The Legend of Code</h1>
-            </Widget.Header>
-            <Widget.Content>
-              <p>lorem ipsum dolor sit amet...</p>
-            </Widget.Content>
-          </Widget>
+        <QuizBackground backgroundImage={db.bg}>
+          <Header />
+          <QuizContainer>
             <Widget>
               <Widget.Header>
-                <h1>Quizes da Galera</h1>
+                <h1>The Legend of Code</h1>
               </Widget.Header>
               <Widget.Content>
-              <p>lorem ipsum dolor sit amet...</p>
-            </Widget.Content>
-          </Widget>
-          <Footer />
-        </QuizContainer>
-        <GitHubCorner projectUrl={'https://github.com/Mathtzt'} />
-      </QuizBackground>
+                <p>lorem ipsum dolor sit amet...</p>
+              </Widget.Content>
+            </Widget>
+              <Widget>
+                <Widget.Header>
+                  <h1>Quizes da Galera</h1>
+                </Widget.Header>
+                <Widget.Content>
+                <p>lorem ipsum dolor sit amet...</p>
+              </Widget.Content>
+            </Widget>
+            <Footer />
+          </QuizContainer>
+          <GitHubCorner projectUrl={'https://github.com/Mathtzt'} />
+        </QuizBackground>
   )
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Head from 'next/head'
+import db from '../../../db.json';
+
+export default function Header() {
+    return (
+        <div>
+            <Head>
+                <title>Quiz - Imersão React</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta property="og:title" content="Quiz - Imersão React" />
+                <meta property="og:description" content="Quiz criado durante a Imersão React Next.js." />
+                <meta property="og:image" content={db.bg} />
+                <meta property="og:url" content="https://quiz-base.mathtzt.vercel.app/" />
+            </Head>
+        </div>
+    );
+}
