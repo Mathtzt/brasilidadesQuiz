04dde854f624d468f721bc599ee380fc411b38c7
Mathtzt <matheus.cn10@gmail.com>
2021-01-28 15:45:57 -0300

    Alterando imagem da meta tag [correcao]

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,7 +10,7 @@ export default function Header() {
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 <meta property="og:title" content="Quiz - Imersão React" />
                 <meta property="og:description" content="Quiz criado durante a Imersão React Next.js." />
-                <meta property="og:image" content={db.bg} />
+                <meta property="og:image" content={db.bgMeta} />
                 <meta property="og:url" content="https://quiz-base.mathtzt.vercel.app/" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
